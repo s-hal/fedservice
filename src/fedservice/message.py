5e7e75ab211206c03e46eaa9d12046c5868b5ab9
Stefan Halen <stefan.halen@iis.se>
2026-07-09 15:19:42 +0200

    Fix

--- src/fedservice/message.py
+++ src/fedservice/message.py
@@ -31,7 +31,6 @@ from idpyoidc.message.oidc import RegistrationResponse
 from idpyoidc.message.oidc import SINGLE_OPTIONAL_BOOLEAN
 from idpyoidc.message.oidc import SINGLE_OPTIONAL_DICT
 
-from fedservice import get_payload
 from fedservice.exception import UnknownCriticalExtension
 from fedservice.exception import WrongSubject
 
