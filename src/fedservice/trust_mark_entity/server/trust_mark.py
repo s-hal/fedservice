ba948bc89bd740fe62a8fabf3cccd4157346bc99
Stefan Halen <stefan.halen@iis.se>
2026-08-11 15:32:33 +0200

    Derive Trust Mark content type from profile

--- src/fedservice/trust_mark_entity/server/trust_mark.py
+++ src/fedservice/trust_mark_entity/server/trust_mark.py
@@ -31,7 +31,7 @@ class TrustMark(Endpoint):
     name = "trust_mark"
     endpoint_name = 'federation_trust_mark_endpoint'
     response_format = "jose"
-    response_content_type = "application/trust-mark+jwt"
+    response_content_type = TRUST_MARK.content_type
 
     def __init__(self,
                  upstream_get: Callable,
